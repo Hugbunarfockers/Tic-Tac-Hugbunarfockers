1bbc95aabbf02959b8b8e900fd528df7aa5e4832
HaraldurShoshan <haraldurshoshan@gmail.com>
2016-10-25 11:49:17 +0000

    GameService.java miplemented not finished, GameServiceTest.java created

--- src/main/java/com/hugbunarfockers/gitcatgo/services/GameService.java
+++ src/main/java/com/hugbunarfockers/gitcatgo/services/GameService.java
@@ -5,12 +5,14 @@ public class GameService
   private GameBoard ticTacToe;
   private Player player1;
   private Player player2;
+  private Player currentPlayer;
 
   public GameService(Gameboard board, Player p1, Player p2)
   {
     ticTacToe = board;
     player1 = p1;
     player2 = p2;
+    currentPlayer = p1;
   }
 
   public void playGame()
@@ -25,4 +27,63 @@ public class GameService
   {
 
   }
+
+  // private boolean playable()
+  // {
+  //   int[][] board = ticTacToe.getBoard();
+  //   int boardSize = ticTacToe.getSize();
+  //   int count = 0;
+
+  //   for(int i = 0; i < ticTacToe.getSize(); i++)
+  //   {
+  //       for(int j = 0; j < ticTacToe.getSize(); j++)
+  //       {
+  //           if(board[i][j] == "x" || board[i][j] == "o")
+  //           {
+  //               count++;
+  //           }
+  //       }
+  //   }
+
+  //   if(count > 0)
+  //   {
+  //       return true;
+  //   }
+  //   else
+  //   {
+  //       return false;
+  //   }
+
+  // }
+
+  private void changeCurrentPlayer()
+  {
+    if(currentPlayer == p1)
+    {
+        currentPlayer = p2;
+    }
+    else
+    {
+        currentPlayer = p1;
+    }
+  }
+
+  private void fillBoard()
+  {
+    int value = 1;
+    int boardSize = ticTacToe.getSize();
+    
+    for(int i = 0; i < boardSize; i++)
+    {
+        for(int j = 0; j < boardSize; j++)
+        {
+            ticTacToe.setValue(i, j, value);
+            value++;
+        }
+    }
+  }
+
+
+
+
 }
