3c3a74ede19102caaaa79d77d640121931a1fa85
Hilmar Tryggvason <hilmart@gmail.com>
2016-10-28 19:26:47 +0000

    Added Spark

--- src/main/java/com/hugbunarfockers/gitcatgo/GitCatGo.java
+++ src/main/java/com/hugbunarfockers/gitcatgo/GitCatGo.java
@@ -1,13 +1,26 @@
 package com.hugbunarfockers.gitcatgo;
 
-public class GitCatGo
-{
-	public static String HelloWorld(){
-		return "Hello World!";
-	}
+import spark.*;
+import static spark.Spark.*;
+import spark.servlet.SparkApplication;
 
-	public static void main(String[] args)
-	{
-		System.out.println(GitCatGo.HelloWorld());
-	}
+public class GitCatGo implements SparkApplication {
+
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        SparkApplication gitCatGo = new GitCatGo();
+
+        String port = System.getenv("PORT");
+        if (port != null) {
+            port(Integer.valueOf(port));
+        }
+
+        gitCatGo.init();
+    }
+
+    @Override
+    public void init() {
+        // TODO
+    }
+
 }
