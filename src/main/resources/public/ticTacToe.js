96c714688fcb6671848d77356ab5553a0e454ebf
Hilmar Tryggvason <hilmart@gmail.com>
2016-10-29 14:22:09 +0000

    Christian is having internet problems, so we used sneakernet to transfer files to me and I push them for him

--- src/main/resources/public/ticTacToe.js
+++ src/main/resources/public/ticTacToe.js
@@ -1,21 +1,47 @@
 $(document).ready(function(){
 
-   	$("#playBtn").click(function(){
-        $("#board").removeClass("hidden");
-        $("#players").addClass("hidden");
+    $("#playBtn").attr('disabled', true);
+
+    $("#playerForm").change(function(){
+        var player1NameStatus = $("#player1name").val();
+        var player1KeyStatus = $("#player1key").val();
+        var player2NameStatus = $("#player2name").val();
+        var player2KeyStatus = $("#player2key").val();
+
+        if (!player1NameStatus || !player1KeyStatus
+            || !player2NameStatus || !player2KeyStatus) {
+            $("#playBtn").attr('disabled', true);
+        }
+        else {
+            $("#playBtn").attr('disabled', false);
+        }
     });
 
-   	
-   	$("#cell1").mouseenter(function(){
-   		$("#cell1").addClass("hoverClass");
-   	});	
-   	
-	$("#cell1").mouseleave(function(){
-   		$("#cell1").removeClass("hoverClass");
-   	});
+    $("#playBtn").click(function(){
+        $("#board").removeClass("hidden");
+        $("#players").addClass("hidden");
+        return false;
+    });
 
- 
+    var thisId;
+    $(".available").hover(
+        function(){
+            thisId = this.id;
 
+            $(this).addClass("hoverClass");
+        },
+        function () {
+            $(this).removeClass("hoverClass");
+        }
+    );
 
-
+    $(".available").click(function(){
+        if( $(this).hasClass("unavailable")){
+            $("#occupied").removeClass("hidden");
+        }
+        else{
+            $(this).addClass("unavailable");
+            $("#occupied").addClass("hidden");
+        }
+    });
 });
