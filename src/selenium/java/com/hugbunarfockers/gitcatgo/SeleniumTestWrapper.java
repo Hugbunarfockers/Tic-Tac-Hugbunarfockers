c183db722897a6e59cb7dd073da0cea1f924b071
Hilmar Tryggvason <hilmart@gmail.com>
2016-10-29 15:25:47 +0000

    Added PhantomJS. It works. I did not remove chrome driver or firefox, so that people can debug locally with their browsers

--- src/selenium/java/com/hugbunarfockers/gitcatgo/SeleniumTestWrapper.java
+++ src/selenium/java/com/hugbunarfockers/gitcatgo/SeleniumTestWrapper.java
@@ -4,19 +4,22 @@ import java.util.concurrent.TimeUnit;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public abstract class SeleniumTestWrapper {
-    static ChromeDriver driver;
+    //static ChromeDriver driver;
+    static PhantomJSDriver driver;
     static String baseUrl;
     static String port;
 
     @BeforeClass
     public static void openBrowser(){
-        driver = new ChromeDriver();
+        //driver = new ChromeDriver();
+        driver = new PhantomJSDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 		port = System.getenv("PORT");
