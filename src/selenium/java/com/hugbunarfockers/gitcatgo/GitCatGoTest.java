ee38371f2e17686b78163622501e0cb5b9072489
Hilmar Tryggvason <hilmart@gmail.com>
2016-10-30 22:48:09 +0000

    Marking two square works

--- src/selenium/java/com/hugbunarfockers/gitcatgo/GitCatGoTest.java
+++ src/selenium/java/com/hugbunarfockers/gitcatgo/GitCatGoTest.java
@@ -103,4 +103,61 @@ public class GitCatGoTest extends SeleniumTestWrapper
 
         assertEquals(true, cell1paragraph.getText().contains("X"));
     }
+
+    @Test
+    public void markTowSquares()
+    {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        driver.get(baseUrl);
+
+        WebElement board = driver.findElement(By.id("board"));
+
+        assertEquals(false, board.isDisplayed());
+
+		String player1Name = "Player1Test3";
+        String player1Key = "player1";
+
+		String player2Name = "Player2Test3";
+        String player2Key = "player2";
+
+        // Login
+
+        WebElement player1NameInput = driver.findElement(By.id("player1name"));
+        player1NameInput.sendKeys(player1Name);
+
+        WebElement player1KeyInput = driver.findElement(By.id("player1key"));
+        player1KeyInput.sendKeys(player1Key);
+
+        WebElement player2NameInput = driver.findElement(By.id("player2name"));
+        player2NameInput.sendKeys(player2Name);
+
+        WebElement player2KeyInput = driver.findElement(By.id("player2key"));
+        player2KeyInput.sendKeys(player2Key);
+
+        // Submit
+        player2KeyInput.sendKeys(Keys.ENTER);
+
+        // Wait
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("board")));
+
+        WebElement cell5 = driver.findElement(By.id("cell5"));
+        cell5.click();
+
+        WebElement cell5paragraph = driver.findElement(By.cssSelector("#cell5 > p"));
+
+        // Wait
+        wait.until(ExpectedConditions.attributeContains(By.id("cell5"), "class", "unavailable"));
+
+        assertEquals(true, cell5paragraph.getText().contains("X"));
+
+        WebElement cell9 = driver.findElement(By.id("cell9"));
+        cell9.click();
+
+        WebElement cell9paragraph = driver.findElement(By.cssSelector("#cell9 > p"));
+
+        // Wait
+        wait.until(ExpectedConditions.attributeContains(By.id("cell9"), "class", "unavailable"));
+
+        assertEquals(true, cell9paragraph.getText().contains("O"));
+    }
 }
