1a6544267a6bcf75a724be71b0105d5a47d070f5
Hilmar Tryggvason <hilmart@gmail.com>
2016-10-30 22:47:54 +0000

    Marking one square works

--- src/selenium/java/com/hugbunarfockers/gitcatgo/GitCatGoTest.java
+++ src/selenium/java/com/hugbunarfockers/gitcatgo/GitCatGoTest.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.util.List;
 
 public class GitCatGoTest extends SeleniumTestWrapper
 {
@@ -54,5 +55,52 @@ public class GitCatGoTest extends SeleniumTestWrapper
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("board")));
 
         assertEquals(true, board.isDisplayed());
+    }
+
+    @Test
+    public void markOneSquare()
+    {
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        driver.get(baseUrl);
+
+        WebElement board = driver.findElement(By.id("board"));
+
+        assertEquals(false, board.isDisplayed());
+
+		String player1Name = "Player1Test2";
+        String player1Key = "player1";
+
+		String player2Name = "Player2Test2";
+        String player2Key = "player2";
+
+        // Login
+
+        WebElement player1NameInput = driver.findElement(By.id("player1name"));
+        player1NameInput.sendKeys(player1Name);
+
+        WebElement player1KeyInput = driver.findElement(By.id("player1key"));
+        player1KeyInput.sendKeys(player1Key);
+
+        WebElement player2NameInput = driver.findElement(By.id("player2name"));
+        player2NameInput.sendKeys(player2Name);
+
+        WebElement player2KeyInput = driver.findElement(By.id("player2key"));
+        player2KeyInput.sendKeys(player2Key);
+
+        // Submit
+        player2KeyInput.sendKeys(Keys.ENTER);
+
+        // Wait
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("board")));
+
+        WebElement cell1 = driver.findElement(By.id("cell1"));
+        cell1.click();
+
+        WebElement cell1paragraph = driver.findElement(By.cssSelector("#cell1 > p"));
+
+        // Wait
+        wait.until(ExpectedConditions.attributeContains(By.id("cell1"), "class", "unavailable"));
+
+        assertEquals(true, cell1paragraph.getText().contains("X"));
     }
 }
