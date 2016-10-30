c9c6c7e4abe1d3534c1c964f377171b8b63ff99f
Unnur Sól Ingimarsdóttir <unnuri15@ru.is>
2016-10-30 21:13:26 +0000

    Entities tests are orcfreakingamazingly successful ^.^

--- src/test/java/com/hugbunarfockers/gitcatgo/entities/PlayerTest.java
+++ src/test/java/com/hugbunarfockers/gitcatgo/entities/PlayerTest.java
@@ -6,10 +6,28 @@ import org.junit.Test;
 public class PlayerTest
 {
 	@Test
+	public void testEmptyConstructor()
+	{
+		Player newPlayer = new Player();
+		assertEquals(0, newPlayer.getID());
+		assertEquals(null, newPlayer.getName());
+		assertEquals(null, newPlayer.getKey());
+	}
+
+	@Test
 	public void testGetID()
 	{
 		Player newPlayer = new Player(1, "Sigrún", "666");
 		assertEquals(1, newPlayer.getID());
+	}
+
+	@Test
+	public void testSetID()
+	{
+		Player newPlayer = new Player();
+		int ID = 10;
+		newPlayer.setID(ID);
+		assertEquals(10, newPlayer.getID());
 	}
 
 	@Test
@@ -20,9 +38,27 @@ public class PlayerTest
 	}
 
 	@Test
+	public void testSetName()
+	{
+		Player newPlayer = new Player();
+		String name = "Unnur";
+		newPlayer.setName(name);
+		assertEquals("Unnur", newPlayer.getName());
+	}
+
+	@Test
 	public void testGetKey()
 	{
 		Player newPlayer = new Player(1, "Sigrún", "666");
+		assertEquals("666", newPlayer.getKey());
+	}
+
+	@Test
+	public void testSetKey()
+	{
+		Player newPlayer = new Player();
+		String key = "666";
+		newPlayer.setKey(key);
 		assertEquals("666", newPlayer.getKey());
 	}
 
